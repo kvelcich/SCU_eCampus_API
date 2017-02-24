922895dd01fafe0ea02ef1fb5461b29be19d09b7
Kevin V <kvelcich@gmail.com>
2017-02-23 23:02:52 -0800

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 actId = ""
 actPwd = ""
 
-#Enter Desination phone number for notifications.
-#Format: +16505550123
-destCellPhone = ""
-
 #Desired course to check, and class to be swapped
 classNum = ""
 #Course to be swapped
 swappedClass = ""
+
+#Enter Desination phone number for notifications.
+#Format: +16505550123
+destCellPhone = ""
 
 #Add Twilio Information for notifications
 accountSid = ""
