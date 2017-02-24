745071e7ee396ed368bfcf3e624afe132e7f1c6b
Kevin Velcich <kvelcich@gmail.com>
2017-02-24 00:43:35 -0800

    Restructuring files

--- values/config.py
+++ values/config.py
@@ -1,12 +1,12 @@
 #Enter ecampus ID and
-actId = "W1106223"
-actPwd = "Belmont_96!"
+actId = ""
+actPwd = ""
 
 #Enter Desination phone number for notifications.
 #Format: +16505550123
-destCellPhone = "+1650888736s"
+destCellPhone = ""
 
 #Add Twilio Information for notifications
-accountSid = "AC4352ad49482ec441936a3d12c7d0cc84"
-authToken = "3f7c0561bb8f6f58e2372afb702cd077"
-myTwilioNumber = "+16507275758"
+accountSid = ""
+authToken = ""
+myTwilioNumber = ""
